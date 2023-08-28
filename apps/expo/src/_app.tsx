d04431963bbf94b248a80f22e6973fb25487dab6
Troy Underwood <troyizzle@gmail.com>
2023-08-28 15:24:28 -0500

    feat(mobile): refactor color scheme provider
    This now supports local theme!

--- apps/expo/src/_app.tsx
+++ apps/expo/src/_app.tsx
@@ -5,40 +5,32 @@ import { SignInSignUpScreen } from "./screens/signin";
 import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { tokenCache } from "./utils/cache";
 import Constants from "expo-constants";
-import { createTheme, ThemeProvider } from "@rneui/themed";
 import ColorSchemeProvider from "./contexts/ColorSchemeProvider";
 import Background from "./components/Background";
 import DefaultNavigation from "./navigation/DefaultNavigation";
-import { useColorScheme } from "react-native";
 import Navigation from "./navigation/Navigation";
 
 export const App = () => {
-  const theme = createTheme({
-    mode: useColorScheme() === "dark" ? "dark" : "light"
-  });
-
   return (
-    <ThemeProvider theme={theme}>
+    <ColorSchemeProvider>
       <Navigation>
-        <ColorSchemeProvider>
-          <Background>
-            <ClerkProvider
-              publishableKey={Constants.expoConfig?.extra?.CLERK_PUBLISHABLE_KEY}
-              tokenCache={tokenCache}
-            >
-              <SignedIn>
-                <TRPCProvider>
-                  <DefaultNavigation />
-                  <StatusBar />
-                </TRPCProvider>
-              </SignedIn>
-              <SignedOut>
-                <SignInSignUpScreen />
-              </SignedOut>
-            </ClerkProvider>
-          </Background>
-        </ColorSchemeProvider>
+        <Background>
+          <ClerkProvider
+            publishableKey={Constants.expoConfig?.extra?.CLERK_PUBLISHABLE_KEY}
+            tokenCache={tokenCache}
+          >
+            <SignedIn>
+              <TRPCProvider>
+                <DefaultNavigation />
+                <StatusBar />
+              </TRPCProvider>
+            </SignedIn>
+            <SignedOut>
+              <SignInSignUpScreen />
+            </SignedOut>
+          </ClerkProvider>
+        </Background>
       </Navigation>
-    </ThemeProvider>
+    </ColorSchemeProvider >
   );
 };
--- apps/expo/src/contexts/ColorSchemeProvider.tsx
+++ apps/expo/src/contexts/ColorSchemeProvider.tsx
@@ -1,23 +1,78 @@
-import { useThemeMode } from "@rneui/themed";
-import { useEffect } from "react";
-import { useColorScheme } from "react-native";
+import { createTheme, ThemeMode, ThemeProvider } from "@rneui/themed";
+import { createContext, useContext, useEffect, useState } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+type ColorSchemeContext = {
+  updateTheme: (theme: ThemeMode) => Promise<void>;
+  themeMode: ThemeMode;
+}
+
+const ColorSchemeContext = createContext({} as ColorSchemeContext);
+
+export function useColorSchemeContext() {
+  return useContext(ColorSchemeContext);
+}
 
 type ColorSchemeProviderProps = {
   children: React.ReactNode;
 };
 
 export default function ColorSchemeProvider({ children }: ColorSchemeProviderProps) {
-  const colorScheme = useColorScheme();
-  const { setMode } = useThemeMode();
+  const [themeMode, setThemeMode] = useState<ThemeMode>("dark");
 
   useEffect(() => {
-    console.log("colorScheme", colorScheme);
-    setMode(colorScheme == "dark" ? "dark" : "light");
-  }, [colorScheme]);
+    const initTheme = async () => {
+      try {
+        let savedTheme = await getTheme();
+
+        if (savedTheme) {
+          setThemeMode(savedTheme as ThemeMode);
+        } else {
+          savedTheme = "dark";
+
+          updateTheme(savedTheme as ThemeMode);
+        }
+
+        setThemeMode(savedTheme as ThemeMode);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    initTheme();
+
+  }, []);
+
+  const updateTheme = async (themeMode: ThemeMode) => {
+    try {
+      await AsyncStorage.setItem("theme", themeMode);
+      setThemeMode(themeMode)
+    } catch (e) {
+      console.log("ThemeContext#updateTheme", e);
+    }
+  }
+
+  const getTheme = async () => {
+    try {
+      return await AsyncStorage.getItem("theme");
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const theme = createTheme({
+    mode: themeMode
+  });
 
   return (
-    <>
+    <ColorSchemeContext.Provider value={{
+      updateTheme,
+      themeMode
+    }}>
+
+      <ThemeProvider theme={theme}>
       {children}
-    </>
+      </ThemeProvider>
+    </ColorSchemeContext.Provider>
   )
 }
