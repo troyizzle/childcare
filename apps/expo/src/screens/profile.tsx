e77a9d53e5a5ab61d1946e2ffb0e590ca183feb1
Troy Underwood <troyizzle@gmail.com>
2023-08-29 15:46:39 -0500

    feat(mobile): add role badges and basic display

--- apps/expo/src/screens/profile.tsx
+++ apps/expo/src/screens/profile.tsx
@@ -1,13 +1,16 @@
 import { useAuth, useUser } from "@clerk/clerk-expo"
-import { Avatar, Button, Text } from "@rneui/themed"
+import { Avatar, Badge, BadgeProps, Button, Text } from "@rneui/themed"
 import { View } from "react-native"
 import ScreenWrapper from "../components/ScreenWrapper"
+import { trpc } from "../utils/trpc"
 
 
 export const ProfileScreen = () => {
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
   const user = useUser().user!
   const { signOut } = useAuth()
+  const userData = trpc.user.byId.useQuery({ id: user.id })
+
 
   return (
     <ScreenWrapper>
@@ -20,14 +23,36 @@ export const ProfileScreen = () => {
                 source={{ uri: user.imageUrl }}
                 rounded
               />
-              <View className="flex flex-col ml-4 mt-2">
-                <Text h4 h4Style={{
-                  fontWeight: "bold",
-                }}>
-                  {user.fullName}</Text>
-                <Text className="text-gray-500">{user?.phoneNumbers?.[0]?.phoneNumber}</Text>
+              <View className="flex flex-col space-y-2 ml-4">
+                <View>
+                  <Text h4 h4Style={{
+                    fontWeight: "bold",
+                  }}>
+                    {user.fullName}
+                  </Text>
+                </View>
+
+                <View className="flex flex-row justify-between">
+                  {userData.data && userData.data.roles.map(({ role }) => (
+                    <RoleBadge key={role.id} value={role.name} role={role.name.toLowerCase() as roleColor} />
+                  ))}
+                </View>
               </View>
             </View>
+
+            {userData.data && userData.data.students.length > 0 && (
+              <View>
+                <Text h4>Students</Text>
+                <Text>{userData.data.students.length}</Text>
+              </View>
+            )}
+
+            {userData.data && userData.data.children.length > 0 && (
+              <View>
+                <Text h4>Children</Text>
+                <Text>{userData.data.children.length}</Text>
+                </View>
+            )}
           </View>
 
           <Button onPress={() => void signOut()}>Sign out</Button>
@@ -35,5 +60,26 @@ export const ProfileScreen = () => {
       </View>
 
     </ScreenWrapper>
+  )
+}
+
+const roleColors = {
+  "admin": "success",
+  "teacher": "warning",
+  "parent": "primary",
+}
+
+type roleColor = keyof typeof roleColors
+
+type RoleBadgeProps = {
+  value: string
+  role: roleColor
+}
+
+function RoleBadge({ value, role }: RoleBadgeProps) {
+  const color = roleColors[role] as BadgeProps["status"]
+
+  return (
+    <Badge value={value} status={color} />
   )
 }
