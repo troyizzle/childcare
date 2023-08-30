b78d25409f206a09626e4fd9fdfcae8786af75e1
Troy Underwood <troyizzle@gmail.com>
2023-08-30 08:09:50 -0500

    feat(web): remove auth showcase

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,13 +1,11 @@
 import { router } from "../trpc";
 import { actionRouter } from "./action";
-import { authRouter } from "./auth";
 import { classroomRouter } from "./classroom";
 import { roleRouter } from "./role";
 import { studentRouter } from "./student";
 import { userRouter } from "./user";
 
 export const appRouter = router({
-  auth: authRouter,
   action: actionRouter,
   classroom: classroomRouter,
   role: roleRouter,
